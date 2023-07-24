1e43c358ae305465e0872d7b57849e8ede476623
Violet <spynetgirl@outlook.com>
2023-07-24 15:06:54 +0100

    Improved Tor browser Sandbox's security
    The host folder for Tor browser is read-only now. When Sandbox launches, Tor browser folder from read-only is quickly copied to a new user-writable folder in Windows Sandbox and then Tor browser is launched from there.

--- Windows Sandboxes/Tor browser/TorBrowser.ps1
+++ Windows Sandboxes/Tor browser/TorBrowser.ps1
@@ -1,13 +1,14 @@
 # Set timezone to a random country
 Set-TimeZone -Id "China Standard Time"
 
+# Since the original Tor folder from host is read-only, we need to copy it to another location otherwise it won't work
+Copy-Item -Recurse -Path 'C:\TorBrowser' -Destination 'C:\TorBrowser2' -Force
+
 # add Tor browser's shortcut to desktop of Windows Sandbox 
-$WshShell = New-Object -comObject WScript.Shell 
-
+$WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\Users\WDAGUtilityAccount\Desktop\TorBrowser.lnk") 
-
-$Shortcut.TargetPath = "C:\TorBrowser\Browser\firefox.exe"
-
+$Shortcut.TargetPath = "C:\TorBrowser2\Browser\firefox.exe"
 $Shortcut.Save() 
 
-& "C:\TorBrowser\Browser\firefox.exe"
+# launch Tor browser
+& "C:\TorBrowser2\Browser\firefox.exe"
