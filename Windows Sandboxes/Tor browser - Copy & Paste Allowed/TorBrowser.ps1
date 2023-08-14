230db497538990a0a67d0d515c586b0ad5809110
HotCakeX <spynetgirl@outlook.com>
2023-08-14 00:05:49 -1000

    Time rotation

--- Windows Sandboxes/Tor browser - Copy & Paste Allowed/TorBrowser.ps1
+++ Windows Sandboxes/Tor browser - Copy & Paste Allowed/TorBrowser.ps1
@@ -1,5 +1,5 @@
 # Set timezone to a random country
-Set-TimeZone -Id "China Standard Time"
+Set-TimeZone -Id "Afghanistan Standard Time"
 
 # Since the original Tor folder from host is read-only, we need to copy it to another location otherwise it won't work
 Copy-Item -Recurse -Path 'C:\TorBrowser' -Destination 'C:\TorBrowser2' -Force
