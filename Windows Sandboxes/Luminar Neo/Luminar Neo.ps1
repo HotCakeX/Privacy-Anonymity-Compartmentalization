8b0f908c97887d3bc02824935fac3fd5c3679ded
Violet <spynetgirl@outlook.com>
2024-04-30 16:36:10 +0300

    updated sandboxes

--- Windows Sandboxes/Luminar Neo/Luminar Neo.ps1
+++ Windows Sandboxes/Luminar Neo/Luminar Neo.ps1
@@ -2,14 +2,14 @@
 # add Luminar apps's shortcut to desktop of Windows Sandbox
 $WshShell = New-Object -comObject WScript.Shell
 
-$Shortcut = $WshShell.CreateShortcut("C:\Users\WDAGUtilityAccount\Desktop\luminar.lnk")
+$Shortcut = $WshShell.CreateShortcut("C:\Users\WDAGUtilityAccount\Desktop\Luminar Neo.lnk")
 
-$Shortcut.TargetPath = "C:\Users\WDAGUtilityAccount\Desktop\luminar\Luminar Neo.exe"
+$Shortcut.TargetPath = "C:\Users\WDAGUtilityAccount\Desktop\Luminar Neo\Luminar Neo.exe"
 
 $Shortcut.Save()
 
 # launches Luminar for the 1st time
-cd "C:\Users\WDAGUtilityAccount\Desktop\luminar" ; .\"Luminar Neo.exe"
+cd "C:\Users\WDAGUtilityAccount\Desktop\Luminar Neo" ; .\"Luminar Neo.exe"
 
 # adds delay to the execution of script, letting Luminar to launch, depending on your hardware lower this value
 
@@ -19,4 +19,4 @@ Start-Sleep -Seconds 22
 taskkill /IM "Luminar Neo.exe" /F
 
 # launches Luminar for the 2nd time, ready to use
-cd "C:\Users\WDAGUtilityAccount\Desktop\luminar" ; .\"Luminar Neo.exe"
+cd "C:\Users\WDAGUtilityAccount\Desktop\Luminar Neo" ; .\"Luminar Neo.exe"
